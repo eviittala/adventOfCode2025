0fa3dd2e0bcb0c315106c83111736e3883497034
Eero Viittala <eero.viittala@outlook.com>
2025-12-03 17:13:17 +0200

    Day 3 task1

--- day3/1.cpp
+++ day3/1.cpp
@@ -18,8 +18,31 @@ std::string getInput() {
     return {};
 }
 
+uint64_t getJoltage(const std::string& str) {
+    uint64_t ret{};
+    const size_t size = str.size();
+
+    for (size_t i{}; i < size; ++i) {
+        for (size_t j{i + 1}; j < size; ++j) {
+            char valStr[2]{};
+            valStr[0] = str[i];
+            valStr[1] = str[j];
+            const uint64_t val = std::stoull(valStr);
+            if (ret < val) {
+                ret = val;
+            }
+        }
+    }
+    return ret;
+}
+
 uint64_t solution(const std::string& input) {
     uint64_t ret{};
+    std::istringstream is{input};
+    for (std::string line; std::getline(is, line);) {
+        // printf("%s\n", line.c_str());
+        ret += getJoltage(line);
+    }
     return ret;
 }
 
