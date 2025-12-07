35a8bb4ada41e567661fcc8a791cbf64acc25740
Eero Viittala <eero.viittala@outlook.com>
2025-12-07 14:32:47 +0200

    Day8 framework

---
+++ day8/1.cpp
@@ -0,0 +1,55 @@
+#include <cassert>
+#include <chrono>
+#include <cstdint>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+
+std::string getInput() {
+    const std::string file = "input.txt";
+    if (std::ifstream is{file, std::ios::ate}) {
+        auto size = is.tellg();
+        std::string str(size, '\0');
+        is.seekg(0);
+        is.read(&str[0], size);
+        return str;
+    }
+    printf("Cannot open file: %s\n", file.c_str());
+    return {};
+}
+
+std::vector<std::string> makeVec(const std::string& input) {
+    std::vector<std::string> ret;
+    std::istringstream is{input};
+    for (std::string line; std::getline(is, line);) {
+        ret.push_back(line);
+    }
+    return ret;
+}
+
+void printVec(const std::vector<std::string>& vec) {
+    for (const auto& line : vec) {
+        std::cout << line << std::endl;
+    }
+}
+
+uint64_t solution(const std::string& input) {
+    uint64_t ret{};
+    auto vec = makeVec(input);
+    return ret;
+}
+
+int main(int argc, char* argv[]) {
+    const auto start = std::chrono::system_clock::now();
+    printf("Answer: %lu\n", solution(getInput()));
+    const auto end = std::chrono::system_clock::now();
+    const std::chrono::duration<double> diff = end - start;
+    std::cout << "Test elapsed: " << diff << std::endl;
+    return 0;
+}
+
