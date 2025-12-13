a965adfccdda08f1ad096d7cba12d182d07a3089
Eero Viittala <eero.viittala@outlook.com>
2025-12-13 22:56:39 +0200

    Day10 task2 trying

--- day10/2.cpp
+++ day10/2.cpp
@@ -95,8 +95,7 @@ void addCounterWithButtons(std::vector<uint64_t>& counter, const size_t idx) {
     counter.at(idx) += 1;
 }
 
-int compare(const std::vector<uint64_t>& expected,
-            const std::vector<uint64_t>& counter) {
+int compare(const std::vector<uint64_t>& expected, const std::vector<uint64_t>& counter) {
     int ret{0};
     for (size_t i{}; i < expected.size(); ++i) {
         if (counter.at(i) < expected.at(i)) {
@@ -108,8 +107,8 @@ int compare(const std::vector<uint64_t>& expected,
     return ret;
 }
 
-std::map<uint64_t, std::vector<std::string>> generateButtonCombinations(
-    const std::vector<std::string>& buttons) {
+std::map<uint64_t, std::vector<std::string>>
+generateButtonCombinations(const std::vector<std::string>& buttons) {
     std::map<uint64_t, std::vector<std::string>> combinations;
 
     for (const auto& button : buttons) {
@@ -123,8 +122,8 @@ std::map<uint64_t, std::vector<std::string>> generateButtonCombinations(
     return combinations;
 }
 
-std::map<uint64_t, std::vector<size_t>> generateButtonCombinationsIdx(
-    const std::vector<std::string>& buttons) {
+std::map<uint64_t, std::vector<size_t>>
+generateButtonCombinationsIdx(const std::vector<std::string>& buttons) {
     std::map<uint64_t, std::vector<size_t>> combinations;
 
     for (size_t idx{}; idx < buttons.size(); ++idx) {
@@ -138,8 +137,7 @@ std::map<uint64_t, std::vector<size_t>> generateButtonCombinationsIdx(
     return combinations;
 }
 
-bool hasExceeded(const std::vector<uint64_t>& joltages,
-                 const std::vector<uint64_t>& pressed,
+bool hasExceeded(const std::vector<uint64_t>& joltages, const std::vector<uint64_t>& pressed,
                  const std::map<uint64_t, std::vector<size_t>>& combinations) {
     for (const auto& [i, combs] : combinations) {
         const auto joltage = joltages.at(i);
@@ -162,10 +160,10 @@ uint64_t getPresses(const std::vector<uint64_t>& presses) {
     return ret;
 }
 
-std::vector<uint64_t> getJoltagesFromPressed(
-    const std::vector<std::string>& buttons,
-    const std::vector<uint64_t>& presses, const size_t size) {
-    std::vector<uint64_t> ret(size);  // TODO Eero
+std::vector<uint64_t> getJoltagesFromPressed(const std::vector<std::string>& buttons,
+                                             const std::vector<uint64_t>& presses,
+                                             const size_t size) {
+    std::vector<uint64_t> ret(size); // TODO Eero
     // printf("Calculating joltages from presses: ");
     // for (const auto press : presses) {
     //     std::cout << press << ", ";
@@ -191,10 +189,10 @@ std::vector<uint64_t> getJoltagesFromPressed(
     return ret;
 }
 
-uint64_t getFewestButtonsPressed(
-    const std::vector<uint64_t>& joltages,
-    const std::vector<std::string>& buttons, std::vector<uint64_t> presses,
-    const std::map<uint64_t, std::vector<size_t>>& combinations) {
+uint64_t getFewestButtonsPressed(const std::vector<uint64_t>& joltages,
+                                 const std::vector<std::string>& buttons,
+                                 std::vector<uint64_t> presses,
+                                 const std::map<uint64_t, std::vector<size_t>>& combinations) {
     if (hasExceeded(joltages, presses, combinations)) {
         return UINT64_MAX;
     }
@@ -208,11 +206,131 @@ uint64_t getFewestButtonsPressed(
     for (size_t idx{}; idx < buttons.size(); ++idx) {
         auto newPresses = presses;
         addCounterWithButtons(newPresses, idx);
-        const auto nbrOfPresses = getFewestButtonsPressed(
-            joltages, buttons, newPresses, combinations);
+        const auto nbrOfPresses =
+            getFewestButtonsPressed(joltages, buttons, newPresses, combinations);
         if (nbrOfPresses < ret) {
             ret = nbrOfPresses;
         }
+    }
+
+    return ret;
+}
+
+uint64_t getNbrOfIdx(const std::map<uint64_t, std::vector<size_t>>& combs) {
+    std::set<uint64_t> ret{};
+    for (const auto& [idx, comb] : combs) {
+        for (const auto& nbr : comb) {
+            ret.insert(nbr);
+        }
+    }
+    return ret.size();
+}
+
+void printNumbers(const std::vector<uint64_t>& numbers) {
+    printf("Numbers(%zu): ", numbers.size());
+    for (size_t i{}; i < numbers.size(); ++i) {
+        printf("%lu", numbers.at(i));
+        if (i < numbers.size() - 1) {
+            printf(", ");
+        }
+    }
+    printf("\n");
+}
+
+bool addNbrs(std::vector<uint64_t>& numbers, const std::vector<uint64_t>& max) {
+    numbers.at(0) += 1;
+    bool endReached{true};
+    for (size_t i{}; i < numbers.size(); ++i) {
+        if (numbers.at(i) < max.at(i)) {
+            endReached = false;
+            break;
+        }
+    }
+    if (endReached) {
+        return false;
+    }
+    if (numbers.at(0) > max.at(0)) {
+        numbers.at(0) = 0;
+        for (size_t i{1}; i < numbers.size(); ++i) {
+            if (++numbers.at(i) > max.at(i)) {
+                numbers.at(i) = 0;
+            } else {
+                if (i == numbers.size() - 3) {
+                    printNumbers(numbers);
+                }
+                break;
+            }
+        }
+    }
+    return true;
+}
+
+uint64_t getNumbers(const std::vector<uint64_t>& numbers) {
+    uint64_t ret{};
+    for (const auto i : numbers) {
+        ret += i;
+    }
+    return ret;
+}
+
+std::vector<uint64_t> getMax(const std::map<uint64_t, std::vector<size_t>>& combinations,
+                             const std::vector<uint64_t>& joltages) {
+    std::vector<uint64_t> ret(getNbrOfIdx(combinations));
+    std::ranges::fill(ret, UINT64_MAX);
+
+    for (const auto& [idx, combs] : combinations) {
+        for (const auto nbr : combs) {
+            if (joltages.at(idx) < ret.at(nbr)) {
+                ret.at(nbr) = joltages.at(idx);
+            }
+        }
+    }
+    return ret;
+}
+void printMax(const std::vector<uint64_t>& numbers) {
+    printf("Max(%zu): ", numbers.size());
+    for (size_t i{}; i < numbers.size(); ++i) {
+        printf("%lu", numbers.at(i));
+        if (i < numbers.size() - 1) {
+            printf(", ");
+        }
+    }
+    printf("\n");
+}
+
+uint64_t getFewestPresses(const std::map<uint64_t, std::vector<size_t>>& combinations,
+                          const std::vector<uint64_t>& joltages) {
+    std::vector<uint64_t> numbers(getNbrOfIdx(combinations));
+    const auto max = getMax(combinations, joltages);
+    printMax(max);
+    uint64_t ret{UINT64_MAX};
+
+    bool run{true};
+
+    while (run) {
+        bool found{true};
+        for (const auto& [idx, comb] : combinations) {
+            uint64_t res{};
+            for (const auto nbr : comb) {
+                res += numbers.at(nbr);
+            }
+            // printf("res: %lu joltages: %lu idx: %lu\n", res, joltages.at(idx), idx);
+            if (res != joltages.at(idx)) {
+                found = false;
+                break;
+            }
+        }
+        if (found) {
+            const uint64_t nbrs = getNumbers(numbers);
+            printf("--FOUND:");
+            printNumbers(numbers);
+            if (nbrs < ret) {
+                ret = nbrs;
+            }
+        }
+
+        // printNumbers(numbers);
+        run = addNbrs(numbers, max);
     }
 
     return ret;
@@ -251,10 +369,10 @@ uint64_t getNbrOfButtonPresses(const std::string& line) {
         printf(" = %lu\n", joltages.at(i));
         // std::cout << std::endl;
     }*/
-    const auto nbrOfPresses =
-        getFewestButtonsPressed(joltages, buttons, pressed, combinations);
+    // const auto nbrOfPresses = getFewestButtonsPressed(joltages, buttons, pressed, combinations);
 
-    return nbrOfPresses;
+    // return nbrOfPresses;
+    return getFewestPresses(combinations, joltages);
 }
 
 uint64_t solution(const std::string& input) {
